f42c5b450fd34699b48be8ab98fa7d00b671ac17
Exequiel Lares <exequiellares@gmail.com>
2023-06-20 17:19:17 -0500

    Fix wrong namespace

--- src/Step/Build/EnableIndexerUseApplicationLock.php
+++ src/Step/Build/EnableIndexerUseApplicationLock.php
@@ -5,7 +5,7 @@
  */
 declare(strict_types=1);
 
-namespace ExequielLares\CloudCustomScenario\Step;
+namespace ExequielLares\CloudCustomScenario\Step\Build;
 
 use Magento\MagentoCloud\App\Error;
 use Magento\MagentoCloud\Filesystem\FileSystemException;
